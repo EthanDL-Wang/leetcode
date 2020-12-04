777283d4a65158220c8d7f7f6866718e6391ad3a
EthanDL-Wang <57888409+EthanDL-Wang@users.noreply.github.com>
2020-12-04 12:23:39 +0800

    20201104hard

--- 42_TrappingRainWater.go
+++ 42_TrappingRainWater.go
@@ -44,3 +44,64 @@ func trap(height []int) int {
     return count
 
 }
+
+
+/***********************************************************************************************************
+*执行用时：4 ms, 在所有 Go 提交中击败了82.89%的用户
+*内存消耗：2.8 MB, 在所有 Go 提交中击败了54.92%的用户
+************************************************************************************************************
+*/
+
+func trap(height []int) int {
+
+    count := 0
+
+    maxIndex := 0
+    for i := 0; i < len(height); i++ {
+        if height[maxIndex] < height[i] {
+            maxIndex = i
+        }
+    }
+
+    leftIndex := maxIndex
+
+    for leftIndex > 0 {
+
+        tmpIndex := 0
+        for i := 0; i < leftIndex; i++ {
+            if height[tmpIndex] < height[i] {
+                tmpIndex = i
+            }
+        }
+
+        for i := tmpIndex+1; i < leftIndex; i++ {
+            count += height[tmpIndex] - height[i]
+            //fmt.Println(i, height[tmpIndex] - height[i])
+        }
+
+        leftIndex = tmpIndex
+        
+    }
+
+    rightIndex := maxIndex
+
+    for rightIndex < len(height)-1 {
+
+        tmpIndex := rightIndex+1
+        for i := tmpIndex; i < len(height); i++ {
+            if height[tmpIndex] < height[i] {
+                tmpIndex = i
+            }
+        }
+
+        for i := rightIndex+1; i < tmpIndex; i++ {
+            count += height[tmpIndex] - height[i]
+            //fmt.Println(i, height[tmpIndex] - height[i])
+        }
+
+        rightIndex = tmpIndex
+        
+    }
+
+    return count
+}
